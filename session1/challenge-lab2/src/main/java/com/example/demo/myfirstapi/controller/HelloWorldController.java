6a4c2ec8393e317ce8c13b83b56e6f5485a8243f
mnljm <jaime.manalo129@gmail.com>
2025-07-16 14:24:01 +0800

    Added challenge-lab2 Fixes

--- session1/challenge-lab2/src/main/java/com/example/demo/myfirstapi/controller/HelloWorldController.java
+++ session1/challenge-lab2/src/main/java/com/example/demo/myfirstapi/controller/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.example.demo.myfirstapi.controller;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // Marks this class as a REST controller
@@ -20,4 +21,17 @@ public class HelloWorldController {
     public String greetUser(@PathVariable String name) { // @PathVariable maps the URL part to this parameter
         return "Greetings, " + name + "!";
     }
+
+    @GetMapping("/greeting")
+    public String greetWithParam(
+            @RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+        return "Hello, " + name + "!";
+    }
+
+    @GetMapping("/calculate/sum")
+    public String sumNumbers(@RequestParam("num1") int number1, @RequestParam("num2") int number2){
+        int sum = number1 + number2;
+        return "The sum of " + number1 + " and " + number2 + " is " + sum;
+    }
+
 }
