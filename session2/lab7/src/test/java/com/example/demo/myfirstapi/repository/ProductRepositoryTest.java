7c9c4b18b48e13f5ba04d5b82bcd1fd847d30901
mnljm <jaime.manalo129@gmail.com>
2025-07-18 15:47:55 +0800

    Added ProductRepositoryTest.java unit tests

--- session2/lab7/src/test/java/com/example/demo/myfirstapi/repository/ProductRepositoryTest.java
+++ session2/lab7/src/test/java/com/example/demo/myfirstapi/repository/ProductRepositoryTest.java
@@ -32,10 +32,19 @@ class ProductRepositoryTest {
 
     @Test
     void findById() {
-        
+        ArrayList products = new ArrayList();
+        final AtomicLong nextId = new AtomicLong(1);
+        products.add(new Product(nextId.getAndIncrement(), "Laptop", 1200.00));
+        products.add(new Product(nextId.getAndIncrement(), "Mouse", 25.50));
+        products.add(new Product(nextId.getAndIncrement(), "Keyboard", 75.00));
+        assertEquals(products.get(0).hashCode(), prodRep.findById(1L).hashCode());
     }
 
     @Test
     void save() {
+        Product gpu = new Product("RTX 5060", 19000);
+        prodRep.save(gpu);
+        assertEquals(gpu.hashCode(), prodRep.findById(4L).hashCode());
+        System.out.println(prodRep.findAll());
     }
 }
